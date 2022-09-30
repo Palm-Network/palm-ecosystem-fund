db1447976cb89a0b66be37e80ecf70a85008b9d8
Meredith Baxter <meredith.baxter@palm.io>
2022-09-30 14:02:23 -0400

    Add config for deploying to Palm network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,22 @@
+require('dotenv').config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "./tasks"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    "palm": {
+      url: `https://palm-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY as string],
+      gasPrice: 10000
+    },
+    "palm-testnet": {
+      url: `https://palm-testnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY as string],
+      gasPrice: 10000
+    }
+  }
 };
 
 export default config;
